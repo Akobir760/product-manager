7f02408f00aff3bfb8b47d954c456189ffe29429
Akobir <akobirortiqov40@gmail.com>
2025-07-14 23:27:52 +0500

    fix

--- products/models.py
+++ products/models.py
@@ -3,9 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Products(models.Model):
+    image = models.ImageField(upload_to='products/')
     name = models.CharField(max_length=200)
     quantity = models.CharField(max_length=200)
+    description = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return f"ID {self.id}) {self.name}  |  {self.quantity}"
+    
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
